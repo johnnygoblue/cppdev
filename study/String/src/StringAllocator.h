39ddd137e6c9180170fda029067b04f033520f5c
Johnny <chanyaoc@gmail.com>
2024-07-03 22:13:13 +0800

    Add second arg in deallocate to conform to std::allocator

--- study/String/src/StringAllocator.h
+++ study/String/src/StringAllocator.h
@@ -3,22 +3,25 @@
 
 // Define an abstract base class so that we can force exception during memory allocation and
 // test whether appropriate exceptions are thrown when such exceptions do happen.
-class StringAllocator {
+class MyAllocator {
 public:
+    using value_type = char;  // fixed type, no need to templatize it
 	// ensure destructor of derived class is called when an object is deleted through a pointer
 	// to the base class.
-    virtual ~StringAllocator() = default;
-    virtual char* allocate(std::size_t size) = 0;
-    virtual void deallocate(char* ptr) = 0;
+    virtual ~MyAllocator() = default;
+    virtual char* allocate(std::size_t size) const = 0;
+    virtual void deallocate(char* ptr, std::size_t size) const = 0;
 };
 
-class DefaultStringAllocator : public StringAllocator {
+class DefaultStringAllocator : public MyAllocator {
 public:
-    char* allocate(std::size_t size) override {
+    DefaultStringAllocator() = default;
+    char* allocate(std::size_t size) const override {
         return new char[size];
     }
-    void deallocate(char* ptr) override {
-        delete[] ptr;
+
+    void deallocate(char* ptr, std::size_t size) const override {
+        ::operator delete(ptr, size * sizeof(char));
     }
 };
 
